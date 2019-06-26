ef8a0f12b7442780bebec371f08368cea842793a
Michael Douchin <mdouchin@3liz.com>
2019-06-26 09:29:02 +0200

    Add spatial test data

--- install/sql/90_glossary.sql
+++ install/sql/90_glossary.sql
@@ -3,7 +3,7 @@ SET search_path TO gobs,public;
 -- indicator
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'second', 'Second', 'Second resolution', 1);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'minute', 'Minute', 'Minute resolution', 2);
-INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'hour', 'hour', 'Hour resolution', 3);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'hour', 'Hour', 'Hour resolution', 3);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'day', 'Day', 'Day resolution', 4);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'month', 'Month', 'Month resolution', 5);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_date_format', 'year', 'Year', 'Year resolution', 6);
@@ -15,3 +15,11 @@ INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VAL
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_value_type', 'date', 'Date', 'Date', 4);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_value_type', 'timestamp', 'Timestamp', 'Timestamp', 5);
 INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('id_value_type', 'boolean', 'Boolean', 'Boolean', 6);
+
+-- sl_geometry_type
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'point', 'Point', 'Simple point geometry', 1);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'multipoint', 'MultiPoint', 'Multi point geometry', 2);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'linestring', 'Linestring', 'Simple linestring geometry', 3);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'multilinestring', 'MultiLinestring', 'Multi linestring geometry', 4);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'polygon', 'Polygon', 'Simple polygon', 5);
+INSERT INTO glossary (gl_field, gl_code, gl_label, gl_description, gl_order) VALUES ('sl_geometry_type', 'multipolygon', 'MultiPolygon', 'Multi Polygon geometry', 6);
--- install/sql/99_test_data.sql
+++ install/sql/99_test_data.sql
@@ -10,10 +10,15 @@ INSERT INTO gobs.actor (id, a_name, a_description, a_email, id_category) VALUES 
 https://www.cirad.fr/en/home-page', 'contact@cirad.fr', 2);
 
 -- indicator
-INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit) VALUES (1, 'pluviometry', 'Monthly pluviometry', 'Monthly rainfall pluviometry in millimetre', 'month', '{pluviometry}', '{Pluviometry}', 'integer', 'mm');
+INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit) VALUES (1, 'pluviometry', 'Daily pluviometry ', 'Daily rainfall pluviometry in millimetre', 'day', '{pluviometry}', '{Pluviometry}', 'integer', 'mm');
+
+-- spatial_layer
+INSERT INTO gobs.spatial_layer (id, sl_code, sl_label, sl_description, sl_creation_date, fk_id_actor, sl_geometry_type) VALUES (1, 'pluviometers', 'Pluviometers', 'Sites equiped with pluviometers to mesure rainfalls', '2019-06-26', 2, 'point');
+
 
 
 -- SEQUENCES
 SELECT pg_catalog.setval('gobs.actor_category_id_seq', 2, true);
 SELECT pg_catalog.setval('gobs.actor_id_seq', 2, true);
 SELECT pg_catalog.setval('gobs.indicator_id_seq', 1, true);
+SELECT pg_catalog.setval('gobs.spatial_layer_id_seq', 1, true);
