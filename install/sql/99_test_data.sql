7e79f316eea660551b10e8f6f3337852da620269
Michael Douchin <mdouchin@3liz.com>
2019-07-16 16:29:58 +0200

    Add French Britany demo data

--- install/sql/99_test_data.sql
+++ install/sql/99_test_data.sql
@@ -8,31 +8,30 @@ INSERT INTO gobs.actor (id, a_name, a_description, a_email, id_category) VALUES 
 INSERT INTO gobs.actor (id, a_name, a_description, a_email, id_category) VALUES (2, 'CIRAD', 'The French agricultural research and international cooperation organization working for the sustainable development of tropical and Mediterranean regions.
 
 https://www.cirad.fr/en/home-page', 'contact@cirad.fr', 2);
+INSERT INTO gobs.actor (id, a_name, a_description, a_email, id_category) VALUES (3, 'DREAL Bretagne', 'Direction régionale de l''environnement, de l''aménagement et du logement, région Bretagne.', 'email@dreal.fr', 1);
 
 -- indicator
-INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit, id_paths) VALUES (1, 'pluviometry', 'Daily pluviometry ', 'Daily rainfall pluviometry in millimetre', 'day', '{pluviometry}', '{Pluviometry}', 'integer', 'mm', 'Environment / Water / Data | Physical and chemical conditions / Water ');
-INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit, id_paths) VALUES (2, 'subdistrict_population', 'Subdistrict population ', 'Number of inhabitants for each subdistrict', 'year', '{population}', '{Population}', 'integer', 'people', 'Socio-eco / Demography / Population ');
+INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit, id_paths) VALUES (1, 'pluviometry', 'Hourly pluviometry ', 'Hourly rainfall pluviometry in millimetre', 'hour', '{pluviometry}', '{Pluviometry}', 'integer', 'mm', 'Environment / Water / Data | Physical and chemical conditions / Water ');
+INSERT INTO gobs.indicator (id, id_label, id_title, id_description, id_date_format, id_value_code, id_value_name, id_value_type, id_value_unit, id_paths) VALUES (2, 'population', 'Population ', 'Number of inhabitants for city', 'year', '{population}', '{Population}', 'integer', 'people', 'Socio-eco / Demography / Population ');
 
 -- protocol
-INSERT INTO gobs.protocol (id, pr_code, pr_name, pr_description) VALUES (1, 'cirad-pluviometry', 'Pluviometry', 'Measure of rainfall in mm
-Material : ...');
-INSERT INTO gobs.protocol (id, pr_code, pr_name, pr_description) VALUES (2, 'cirad-population', 'Population', 'Number of inhabitants obtained from census forms.');
+INSERT INTO gobs.protocol (id, pr_code, pr_name, pr_description) VALUES (1, 'cirad-pluviometry', 'Pluviometry', 'Measure of rainfall in mm');
+INSERT INTO gobs.protocol (id, pr_code, pr_name, pr_description) VALUES (2, 'cirad-population', 'Population', 'Number of inhabitants obtained from census.');
 
 
 -- spatial_layer
 INSERT INTO gobs.spatial_layer (id, sl_code, sl_label, sl_description, sl_creation_date, fk_id_actor, sl_geometry_type) VALUES (1, 'pluviometers', 'Pluviometers', 'Sites equiped with pluviometers to mesure rainfalls', '2019-06-26', 2, 'point');
-INSERT INTO gobs.spatial_layer (id, sl_code, sl_label, sl_description, sl_creation_date, fk_id_actor, sl_geometry_type) VALUES (2, 'montpellier-districts', 'Montpellier districts', 'Districts of Montpellier, France', '2019-07-05', 2, 'multipolygon');
-INSERT INTO gobs.spatial_layer (id, sl_code, sl_label, sl_description, sl_creation_date, fk_id_actor, sl_geometry_type) VALUES (3, 'montpellier-sub-districts', 'Montpellier Sub-districts', 'Sub-districts of Montpellier, France', '2019-07-15', 2, 'multipolygon');
+INSERT INTO gobs.spatial_layer (id, sl_code, sl_label, sl_description, sl_creation_date, fk_id_actor, sl_geometry_type) VALUES (2, 'brittany-cities', 'Cities of Brittany , France', 'Cities of Brittany, France', '2019-07-05', 2, 'multipolygon');
 
 -- series
 INSERT INTO gobs.series (id, fk_id_protocol, fk_id_actor, fk_id_indicator, fk_id_spatial_layer) VALUES (1, 1, 2, 1, 1);
-INSERT INTO gobs.series (id, fk_id_protocol, fk_id_actor, fk_id_indicator, fk_id_spatial_layer) VALUES (2, 2, 2, 2, 3);
+INSERT INTO gobs.series (id, fk_id_protocol, fk_id_actor, fk_id_indicator, fk_id_spatial_layer) VALUES (2, 2, 2, 2, 2);
 
 -- SEQUENCES
-SELECT pg_catalog.setval('gobs.actor_category_id_seq', 2, true);
-SELECT pg_catalog.setval('gobs.actor_id_seq', 2, true);
+SELECT pg_catalog.setval('gobs.actor_category_id_seq', 3, true);
+SELECT pg_catalog.setval('gobs.actor_id_seq', 3, true);
 SELECT pg_catalog.setval('gobs.indicator_id_seq', 2, true);
 SELECT pg_catalog.setval('gobs.protocol_id_seq', 2, true);
-SELECT pg_catalog.setval('gobs.spatial_layer_id_seq', 3, true);
+SELECT pg_catalog.setval('gobs.spatial_layer_id_seq', 2, true);
 SELECT pg_catalog.setval('gobs.series_id_seq', 2, true);
 
