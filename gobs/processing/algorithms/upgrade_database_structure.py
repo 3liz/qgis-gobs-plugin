e0453fb526601edbac0795e52b8019940d27133f
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-05-11 10:35:16 +0200

    review headers

--- gobs/processing/algorithms/upgrade_database_structure.py
+++ gobs/processing/algorithms/upgrade_database_structure.py
@@ -1,21 +1,7 @@
-"""
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
-
-__author__ = '3liz'
-__date__ = '2019-02-15'
-__copyright__ = '(C) 2019 by 3liz'
-
-# This will get replaced with a git SHA1 when you do a git archive
-
-__revision__ = '$Format:%H$'
+__copyright__ = "Copyright 2020, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
 
 import configparser
 import os
